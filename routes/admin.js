60a941dfbe6a458d30bf49f5d26b3dad7329a7d7
unknown <caionsantos2000@gmail.com>
2019-12-06 14:10:06 -0300

    READ categorias

--- routes/admin.js
+++ routes/admin.js
@@ -12,8 +12,14 @@ router.get('/posts',(req,res)=>{
     res.send('Pagina de posts')
 })
 
+// read
 router.get('/categorias',(req,res)=>{
-    res.render('admin/categorias')
+    Categoria.find().sort({date:'desc'}).then((categorias)=>{
+        res.render('admin/categorias',{categorias:categorias})
+    }).catch((erro)=>{
+        req.flash("error_msg","Houve um erro")
+        res.redirect('/admin')
+    })
 })
 
 router.get('/categorias/add',(req,res)=>{
