de77fa3b3c2933dc2c9e37e12bf7b0be52052b15
unknown <caionsantos2000@gmail.com>
2019-12-06 13:28:49 -0300

    add validation alert addcategorias

--- routes/admin.js
+++ routes/admin.js
@@ -20,17 +20,37 @@ router.get('/categorias/add',(req,res)=>{
     res.render('admin/addcategorias')
 })
 
-router.post('/categorias/add',(req,res)=>{
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
+router.post('/categorias/nova',(req,res)=>{
+    var erros = [];
+
+    if(!req.body.nome || typeof req.body.nome == undefined|| req.body.nome == null){
+        erros.push({texto: "Nome invalido"})
     }
-    
-    new Categoria(novaCategoria).save().then(()=>{
-        console.log("Categoria salva com sucesso")
-    }).catch((err)=>{
-        console.log('Erro ao criar nova categoria' +err)
-    })
+
+    if(req.body.nome.length < 2){
+        erros.push({texto:"Nome da categoria muito pequeno"})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined|| req.body.slug == null){
+        erros.push({texto: "Slug invalido"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias",{erros: erros})
+    }else{
+        const novaCategoria = {
+            nome: req.body.nome,
+            slug: req.body.slug
+        }
+        
+        new Categoria(novaCategoria).save().then(()=>{
+            req.flash("success_msg","Categoria criada com sucesso")
+            res.redirect('/admin/categorias')
+        }).catch((err)=>{
+            req.flash("error_msg","Falha ao criar a categoria")
+            res.redirect('/adim')
+        })
+    }
       
 })
 
